dab5a522eb103757f60e940644d59c48ee2ded57
dnomadb <damon@mapbox.com>
2015-01-12 20:13:47 -0800

    adding command to cli

--- gribnormalize/scripts/cli.py
+++ gribnormalize/scripts/cli.py
@@ -13,4 +13,12 @@ def cli():
 def unwrap(inputgrib, output, bandtags):
     csg.upwrap_raster(inputgrib, output, bandtags)
 
+@click.command('clipdatecross')
+@click.argument('inputgrib', type=str)
+@click.argument('output', type=str)
+@click.option('--bandtags', '-bt', is_flag=True,
+    help='Flag to indicate printing of band tags / band metadata to stdout')
+def clipdatecross(inputgrib, output, bandtags):
+    csg.upwrap_raster(inputgrib, output, bandtags)
+
 cli.add_command(unwrap)
