6686aee29cb0a32fb066f19ad1db8ad50765ce7a
dnomadb <damon@mapbox.com>
2015-01-20 10:38:37 -0800

    punting on rap-chopping for now

--- gribdoctor/scripts/cli.py
+++ gribdoctor/scripts/cli.py
@@ -15,19 +15,3 @@ def unwrap(inputgrib, output, bidx, bandtags):
     csg.upwrap_raster(inputgrib, output, bidx, bandtags)
 
 cli.add_command(unwrap)
-
-@click.command('rapclip')
-@click.argument('inputgrib', type=str)
-@click.argument('output', type=str)
-@click.option('--bandtags', '-bt', is_flag=True,
-    help='Flag to indicate printing of band tags / band metadata to stdout')
-@click.option('--bidx', '-b', default='all',
-    help='Bands to include [default = all]')
-def rapclip(inputgrib, output, bidx, bandtags):
-    '''
-    Clips antimeridian-crossing RAP weather rasters
-    '''
-    click.echo('This subcommand is still in development.')
-    # csg.upwrap_raster(inputgrib, output, bandtags)
-
-cli.add_command(rapclip)
