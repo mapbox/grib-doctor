e3f9876ca009be287f565ebfb2efe0198436a36d
dnomadb <damon@mapbox.com>
2015-01-26 16:45:44 -0800

    adding smoosh subcommand

--- gribdoctor/scripts/cli.py
+++ gribdoctor/scripts/cli.py
@@ -5,8 +5,8 @@ def cli():
     pass
 
 @click.command('unwrap')
-@click.argument('inputgrib', type=str)
-@click.argument('output', type=str)
+@click.argument('inputgrib', type=click.Path(exists=True))
+@click.argument('output', type=click.Path(exists=False))
 @click.option('--bidx', '-b', default='all',
     help='Bands to include [default = all]')
 @click.option('--bandtags', '-bt', is_flag=True,
@@ -15,3 +15,12 @@ def unwrap(inputgrib, output, bidx, bandtags):
     csg.upwrap_raster(inputgrib, output, bidx, bandtags)
 
 cli.add_command(unwrap)
+
+@click.command('smoosh')
+@click.argument('inputs', type=click.Path(exists=True), nargs=-1)
+@click.argument('output', type=click.Path(exists=False), nargs=1)
+def smoosh(inputs, output):
+    'stack two global gfs gribs of (possible) varying resolutions'
+    csg.smoosh_rasters(inputs, output)
+
+cli.add_command(smoosh)
